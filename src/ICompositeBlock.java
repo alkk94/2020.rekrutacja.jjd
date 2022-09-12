ff342dec10ed74fe6ff506d342f18530501f6f75
alk <adrianlukasik994@gmail.com>
2022-09-12 21:20:35 +0200

    Renamed class ICompositeBlock

---
+++ src/ICompositeBlock.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public interface ICompositeBlock extends IBlock {
+    List getBlocks();
+}
--- src/ICompositeIBlock.java
+++
@@ -1,5 +0,0 @@
-import java.util.List;
-
-public interface ICompositeIBlock extends IBlock {
-    List getBlocks();
-}
