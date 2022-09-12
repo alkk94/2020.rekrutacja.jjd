6f6319d3c280c5c686f44e831e71a768a132b38c
alk <adrianlukasik994@gmail.com>
2022-09-12 21:17:45 +0200

    removed comments within IStructure

--- src/IStructure.java
+++ src/IStructure.java
@@ -2,12 +2,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IStructure {
-    // zwraca dowolny element o podanym kolorze
     Optional findBlockByColor(String color);
 
-    // zwraca wszystkie elementy z danego materiału
     List findBlocksByMaterial(String material);
 
-    //zwraca liczbę wszystkich elementów tworzących strukturę
     int count();
 }
